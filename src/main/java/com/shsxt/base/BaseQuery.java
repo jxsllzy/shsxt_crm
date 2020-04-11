fa2a833e01655488202789cbae3d3431580bb040
your_username <1298065072@qq.com>
2020-04-11 17:45:09 +0800

    营销管理&机会数据查询&数据展示

--- src/main/java/com/shsxt/base/BaseQuery.java
+++ src/main/java/com/shsxt/base/BaseQuery.java
@@ -1,4 +1,23 @@
 package com.shsxt.base;
 
 public class BaseQuery {
+
+    private int page = 1;
+    private int rows = 10;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
 }
